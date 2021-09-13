66f1b246458465c8a7c5917e68f7a678028ed087
Jim7rfp <Jim7fp@gmail.com>
2021-09-13 11:05:52 +0300

    слегка облагороженный сайт trofeyrt.ru

--- modal-w1.js
+++ modal-w1.js
@@ -2,22 +2,17 @@ let modal = document.querySelectorAll('.overlay');
 let btn = document.querySelectorAll('.my-btn');
 let spClose = document.getElementsByClassName('closex');
 const docBtns = document.querySelectorAll('a[data-document]'),
-  docs = document.querySelectorAll('.document');
+	docs = document.querySelectorAll('.document');
 console.log("js start");
 console.log(docBtns);
 
-
 for (const _btn of btn) {
-  _btn.onclick = function () {
-    console.log(_btn);
-    for (let j = 0; j < modal.length; j++) {
-      modal[j].style.display = "block";
-  
-      
-    }
-    
-  };
-
+	_btn.onclick = function () {
+		console.log(_btn);
+		for (let j = 0; j < modal.length; j++) {
+			modal[j].style.display = "block";
+		}
+	};
 }
 
 // docBtns[0].onclick = function () {
@@ -25,13 +20,12 @@ for (const _btn of btn) {
 // };
 
 
-
 spClose[0].onclick = function () {
-  modal[0].style.display = "none";
+	modal[0].style.display = "none";
 };
 
-  window.onclick = function (event) {
-    if (event.target == modal[0]) {
-      modal[0].style.display = "none";
-    }
-  };
+window.onclick = function (event) {
+	if (event.target === modal[0]) {
+		modal[0].style.display = "none";
+	}
+};
